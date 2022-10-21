aac88bd99759dfe5fd27c95a597816145be6f440
401-ubuntu <240450828@qq.com>
2022-10-21 08:12:30 +0800

    修改ch18.MaybeT

--- src/Ch18/MaybeT.hs
+++ src/Ch18/MaybeT.hs
@@ -1,10 +1,39 @@
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, UndecidableInstances #-}
+
 module Ch18.MaybeT where
 
 import Control.Monad
+import Control.Monad.State
 
+-- https://blog.csdn.net/zwvista/article/details/78167779
 
-newtype MaybeT m a = MaybeT
-  { runMaybeT :: m (Maybe a) }
+newtype MaybeT m a = MaybeT { 
+  runMaybeT :: m (Maybe a) 
+  } 
+
+-- ‘fail’ is not a (visible) method of class ‘Monad
+instance (Monad m) => Monad (MaybeT m) where
+  return = returnMT
+  (>>=) = bindMT
+  -- fail = failMT
+
+instance (Functor m) => Functor (MaybeT m) where
+    fmap f = mapMaybeT (fmap (fmap f))
+
+mapMaybeT :: (m (Maybe a) -> n (Maybe b)) -> MaybeT m a -> MaybeT n b
+mapMaybeT f = MaybeT . f . runMaybeT
+
+instance (Functor m, Monad m) => Applicative (MaybeT m) where
+    pure = lift . return
+    mf <*> mx = MaybeT $ do
+        mb_f <- runMaybeT mf
+        case mb_f of
+            Nothing -> return Nothing
+            Just f  -> do
+                mb_x <- runMaybeT mx
+                case mb_x of
+                    Nothing -> return Nothing
+                    Just x  -> return (Just (f x))
 
 bindMT :: (Monad m) => MaybeT m a -> (a -> MaybeT m b) -> MaybeT m b
 x `bindMT` f = MaybeT $ do
@@ -24,11 +53,12 @@ failMT :: (Monad m) => t -> MaybeT m a
 failMT _ = MaybeT $ return Nothing
 
 
--- TODO ‘fail’ is not a (visible) method of class ‘Monad
--- instance (Monad m) => Monad (MaybeT m) where
---   return = returnMT
---   (>>=) = bindMT
---   fail = failMT
+instance MonadTrans MaybeT where
+    lift m = MaybeT (Just `liftM` m)
 
--- instance MonadTrans MaybeT where
---     lift m = MaybeT (Just `liftM` m)
+instance (MonadIO m) => MonadIO (MaybeT m) where
+  liftIO m = lift (liftIO m)
+
+instance (MonadState s m) => MonadState s (MaybeT m) where
+  get = lift get
+  put k = lift (put k)
