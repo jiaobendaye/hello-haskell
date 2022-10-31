6339c5ee5d023dba3dcfe59ca0b88b591f31bbbd
401-ubuntu <240450828@qq.com>
2022-10-31 08:13:23 +0800

    update ch20

---
+++ src/Ch20/Posixtime.hs
@@ -0,0 +1,18 @@
+module Ch20.Posixtime where
+
+import System.Posix.Files
+import System.Time
+import System.Posix.Types
+
+-- | Given a path, returns (atime, mtime, ctime)
+getTimes :: FilePath -> IO (ClockTime, ClockTime, ClockTime)
+getTimes fp =
+    do stat <- getFileStatus fp
+       return (toct (accessTime stat),
+               toct (modificationTime stat),
+               toct (statusChangeTime stat))
+
+-- | Convert an EpochTime to a ClockTime
+toct :: EpochTime -> ClockTime
+toct et =
+    TOD (truncate (toRational et)) 0
