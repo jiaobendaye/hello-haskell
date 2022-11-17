61a0063863403ffb7facf7dd44ce893d4a913c2b
401-ubuntu <240450828@qq.com>
2022-11-17 08:30:06 +0800

    更新ch04

---
+++ src/Ch04/InteractWith.hs
@@ -0,0 +1,21 @@
+module Ch04.InteractWith(
+  mainF
+) where
+
+import System.Environment (getArgs)
+
+interactWith :: (String -> String) -> FilePath -> FilePath -> IO ()
+interactWith function inputFile outputFile = do
+  input <- readFile inputFile
+  writeFile outputFile (function input)
+
+mainF :: IO ()
+mainF = mainWith myFunction
+  where mainWith function = do
+          args <- getArgs
+          case args of
+            [input,output] -> interactWith function input output
+            _ -> putStrLn "error: exactly two arguments needed"
+
+        -- replace "id" with the name of our function below
+        myFunction = id
---
+++ src/Ch04/SplitLines.hs
@@ -0,0 +1,16 @@
+module Ch04.SplitLines(
+  splitLines
+) where
+
+splitLines :: String -> [String]
+splitLines [] = []
+splitLines cs =
+    let (pre, suf) = break isLineTerminator cs
+    in  pre : case suf of
+                ('\r':'\n':rest) -> splitLines rest
+                ('\r':rest)      -> splitLines rest
+                ('\n':rest)      -> splitLines rest
+                _                -> []
+
+isLineTerminator :: Char -> Bool
+isLineTerminator c = c == '\r' || c == '\n'
